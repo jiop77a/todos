c30a8f1719e0f6615b206f763ec3469e0bcd6d15
Alex Scott <aescott@outlook.com>
2017-05-08 11:22:10 -0700

    initial commit

---
+++ Todos/frontend/todo_redux.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+  <h1>Todos App</h1>
+);
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+  );
+});
