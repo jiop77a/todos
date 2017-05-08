7d83a10bceff131a5329e6543d1353f6394d3446
Alex Scott <aescott@outlook.com>
2017-05-08 16:30:56 -0700

    refactor todoListItem

--- Todos/frontend/components/todo_list/todo_list.jsx
+++ Todos/frontend/components/todo_list/todo_list.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import TodoListItem from './todo_list_item';
 
-const TodoList = props => (
-  <div>
-    <ul>
-      {props.todos.map((todo, idx) => <li key={idx}>{ todo.title }</li>)}
-    </ul>
-  </div>
-);
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    const todoItems = this.props.todos.map(todo => (
+      <TodoListItem key={todo.id} todo={todo} />
+    )
+  );
+
+  return(
+    <div>
+      <ul className="todo-list">
+        { todoItems }
+      </ul>
+    </div>
+  );
+
+  }
+}
+
 
 export default TodoList;
